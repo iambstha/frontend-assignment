c50d97b1df80430f891f5ec13fb11ea76c2f9b88
iambstha <iambstha@gmail.com>
2023-05-31 17:57:24 +0545

    Edited

---
+++ data.js
@@ -0,0 +1,81 @@
+[
+    {
+      album: {
+        album_type: 'single',
+        artists: [
+          {
+            external_urls: { spotify: 'https://open.spotify.com/artist/3t8WiyalpvnB9AObcMufiE' },
+            id: '3t8WiyalpvnB9AObcMufiE',
+            name: 'Mahmut Orhan',
+            type: 'artist',
+            uri: 'spotify:artist:3t8WiyalpvnB9AObcMufiE'
+          }, {
+            external_urls: { spotify: 'https://open.spotify.com/artist/40Hr91B6wn9pO83Gj0JMrP' },
+            id: '40Hr91B6wn9pO83Gj0JMrP',
+            name: 'Ali Arutan',
+            type: 'artist',
+            uri: 'spotify:artist:40Hr91B6wn9pO83Gj0JMrP'
+          },
+          {
+            external_urls: { spotify: 'https://open.spotify.com/artist/5xkqotsRPu6KQ4PiWjSGQf' },
+            id: '5xkqotsRPu6KQ4PiWjSGQf',
+            name: 'Selin',
+            type: 'artist',
+            uri: 'spotify:artist:5xkqotsRPu6KQ4PiWjSGQf'
+          },
+          
+        ],
+        external_urls: { spotify: 'https://open.spotify.com/album/1B68g8b4wpedNDvvQLAoCe' },
+        id: '1B68g8b4wpedNDvvQLAoCe',
+        images: [
+          { height: 640, url: 'https://i.scdn.co/image/ab67616d0000b273fa258529452f4ed34cc961b1', width: 640 }, { height: 300, url: 'https://i.scdn.co/image/ab67616d00001e02fa258529452f4ed34cc961b1', width: 300 },
+          { height: 64, url: 'https://i.scdn.co/image/ab67616d00004851fa258529452f4ed34cc961b1', width: 64 }
+        ],
+        is_playable: true,
+        name: 'In Control (feat. Selin)',
+        release_date: '2020-10-30',
+        release_date_precision: 'day',
+        total_tracks: 1,
+        type: 'album',
+        uri: 'spotify:album:1B68g8b4wpedNDvvQLAoCe'
+      },
+      artists: [
+        {
+          external_urls: { spotify: 'https://open.spotify.com/artist/3t8WiyalpvnB9AObcMufiE' },
+          id: '3t8WiyalpvnB9AObcMufiE',
+          name: 'Mahmut Orhan',
+          type: 'artist',
+          uri: 'spotify:artist:3t8WiyalpvnB9AObcMufiE'
+        }, {
+          external_urls: { spotify: 'https://open.spotify.com/artist/40Hr91B6wn9pO83Gj0JMrP' },
+          id: '40Hr91B6wn9pO83Gj0JMrP',
+          name: 'Ali Arutan',
+          type: 'artist',
+          uri: 'spotify:artist:40Hr91B6wn9pO83Gj0JMrP'
+        },
+        {
+          external_urls: { spotify: 'https://open.spotify.com/artist/5xkqotsRPu6KQ4PiWjSGQf' },
+          id: '5xkqotsRPu6KQ4PiWjSGQf',
+          name: 'Selin',
+          type: 'artist',
+          uri: 'spotify:artist:5xkqotsRPu6KQ4PiWjSGQf'
+        },
+        
+      ],
+      disc_number: 1,
+      duration_ms: 179232,
+      explicit: false,
+      external_ids: { isrc: 'USUS12000579' },
+      external_urls: { spotify: 'https://open.spotify.com/track/4WNcduiCmDNfmTEz7JvmLv' },
+      id: '4WNcduiCmDNfmTEz7JvmLv',
+      is_local: false,
+      is_playable: true,
+      name: 'In Control (feat. Selin)',
+      popularity: 52,
+      preview_url: 
+        'https://p.scdn.co/mp3-preview/1d53b96abb564f9ba08427c3c5361dd8fbe72f7d?cid=d8a5ed958d274c2e8ee717e6a4b0971d',
+      track_number: 1,
+      type: 'track',
+      uri: 'spotify:track:4WNcduiCmDNfmTEz7JvmLv'
+    }
+  ]
